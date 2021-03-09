7af04d71db7627ce76ef4abd03b4e0c50e29ae95
gcrevisy <gcrevisy@outlook.com>
2021-03-09 12:40:28 +0100

    ajout projet test

---
+++ Dashboard.UITests/MainWindowTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dashboard.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.UI.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void MainWindowTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
