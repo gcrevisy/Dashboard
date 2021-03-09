3d9c5791cbcf30d8489459beeff9c75f63b79233
gcrevisy <gcrevisy@outlook.com>
2021-03-09 13:15:59 +0100

    test TU

---
+++ Dashboard.UI/Model/Branche.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.UI.Model
+{
+    public class Branche
+    {
+        public string Nom { get; set; }
+    }
+}
---
+++ Dashboard.UI/Service/GithubService.cs
@@ -0,0 +1,23 @@
+﻿using Dashboard.UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.UI.Service
+{
+    public class GithubService
+    {
+        public GithubService()
+        {
+        }
+
+        public IList<Branche> GetBranches()
+        {
+            IList<Branche> result = new List<Branche>();
+            result.Add(new Branche { Nom = "TOTO" });
+            return result;
+        }
+    }
+}
