004fc1a3cf745ba687ea4dc7c3691e1acfbd3f04
gcrevisy <gcrevisy@outlook.com>
2021-03-10 07:42:14 +0100

    projet test vstest

---
+++ Dashboard.UITests/Service/GithubServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dashboard.UI.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.UI.Service.Tests
+{
+    [TestClass()]
+    public class GithubServiceTests
+    {
+        [TestMethod()]
+        public void GetBranchesTest()
+        {
+            GithubService service = new GithubService();
+            Assert.IsNotNull(service.GetBranches());
+        }
+    }
+}
