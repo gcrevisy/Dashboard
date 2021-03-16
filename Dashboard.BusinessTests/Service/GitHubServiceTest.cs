7a303b1bf9b880853300034f7ced8e1b3d72280b
gcrevisy <gcrevisy@outlook.com>
2021-03-16 07:13:06 +0100

    ajout test

---
+++ Dashboard.BusinessTests/Service/GitHubServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Dashboard.Business.Service;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.BusinessTests.Service
+{
+    public class GitHubServiceTest
+    {
+        [Test]
+        public void ListerBranchesTest()
+        {
+            GitHubService service = new GitHubService();
+
+            Assert.Pass();
+        }
+    }
+}
---
+++ Dashboard.BusinessTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Dashboard.BusinessTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
